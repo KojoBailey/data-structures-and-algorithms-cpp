d40d8d03cbcf771a9576dd54423d76247f4f9c33
Kojo Bailey <kojomolojo@gmail.com>
2026-05-12 21:45:23 +0100

    Create StaticArray class with sequential_search and format compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <print>
 
+#include "static_array.hpp"
+
 int main()
 {
-	std::println("Hello, world!");
+	StaticArray<int, 5> arr = {3, 1, 4, 1, 5};
+	std::println("{}", arr);
+
+	// std::println("{}", arr.sequential_search(3));
+	// std::println("{}", arr.sequential_search(1));
+	// std::println("{}", arr.sequential_search(1, 1));
+	// std::println("{}", arr.sequential_search(4));
+	// std::println("{}", arr.sequential_search(5));
 }
---
+++ src/static_array.hpp
@@ -0,0 +1,56 @@
+#include <cstdint>
+#include <format>
+#include <string>
+
+template<typename T, std::size_t N>
+class StaticArray {
+public:
+	using value_type = T;
+	static constexpr std::size_t size = N;
+
+	value_type _array[size]{};
+
+	value_type operator[](std::size_t index)
+	{
+		return _array[index];
+	}
+
+	auto sequential_search(value_type target, std::size_t target_iteration = 0)
+		-> std::optional<std::size_t>
+	{
+		std::size_t index = 0;
+		std::size_t iteration = 0;
+		for (auto elem : _array) {
+			if (elem == target) {
+				if (iteration == target_iteration) {
+					return index;
+				}
+				iteration++;
+			}
+			index++;
+		}
+
+		return {};
+	}
+};
+
+template<typename T, std::size_t N>
+struct std::formatter<StaticArray<T, N>> {
+	constexpr auto parse(std::format_parse_context& ctx) {
+		return ctx.begin();
+	}
+
+	auto format(const StaticArray<T, N>& obj, std::format_context& ctx) const {
+		std::string str = "";
+		
+		std::size_t i = 0;
+		for (auto elem : obj._array) {
+			str += std::format("{}", elem);
+			if (i != N - 1) { 
+				str += ", ";
+			}
+			i++;
+		}
+		return std::format_to(ctx.out(), "[{}]", str);
+	}
+};
